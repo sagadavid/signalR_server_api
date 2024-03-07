40549fcf3ea6dcb0186ee36533e26ff5a8f435cd
sagadavid <epostsaga@gmail.com>
2024-03-07 17:54:49 +0100

    Hub context and controller

--- SignalR.Server/HubConfig/ChartHub.cs
+++ SignalR.Server/HubConfig/ChartHub.cs
@@ -4,5 +4,7 @@ namespace SignalR.Server.HubConfig
 {
     public class ChartHub : Hub
     {
+        //when hubcontext is oneway, but not a two-way trip, then there is no need for any mehtod.
+       
     }
 }
---
+++ SignalR.Server/Kontroller/ChartController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalR.Server.HubConfig;
+
+namespace SignalR.Server.Kontroller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+        private readonly IHubContext<ChartHub> _hubContext;
+
+        public ChartController(IHubContext<ChartHub> hubContext)
+        {
+            _hubContext = hubContext;
+                
+        }
+    }
+}
