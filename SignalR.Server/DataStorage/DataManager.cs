7a2d70f185a6f4914c77d56bb41faa93ca8da165
sagadavid <epostsaga@gmail.com>
2024-03-07 17:45:17 +0100

    Chart Data managing

---
+++ SignalR.Server/DataStorage/DataManager.cs
@@ -0,0 +1,22 @@
+﻿using SignalR.Server.Models;
+namespace SignalR.Server.DataStorage
+
+{
+    public class DataManager
+    {
+        public static List<ChartModel> GetChartData()
+        {
+            var r = new Random();
+            return new List<ChartModel>
+            {
+
+            new ChartModel {Data = new List<int> {r.Next(0, 100)}, Label = "Temprature"},
+            new ChartModel { Data = new List<int> { r.Next(0, 100) }, Label = "Fugtighet" },
+            new ChartModel {Data = new List<int> {r.Next(0, 100)}, Label = "Precipitaion"},
+            new ChartModel { Data = new List<int> { r.Next(0, 100) }, Label = "Toxication" }
+
+
+            };
+        }
+    }
+}
