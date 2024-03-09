4c19ec5efd4f865af512d7e1a05c173d9b413d52
sagadavid <epostsaga@gmail.com>
2024-03-09 08:20:01 +0100

    some refactoring

--- SignalR.Server/Kontroller/ChartController.cs
+++ SignalR.Server/Kontroller/ChartController.cs
@@ -1,32 +1,1 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SignalR;
-using SignalR.Server.HubConfig;
-using SignalR.Server.TimerFeatures;
-using SignalR.Server.DataStorage;
-
-namespace SignalR.Server.Kontroller
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ChartController : ControllerBase
-    {
-        private readonly IHubContext<ChartHub> _hubContext;
-
-        public ChartController(IHubContext<ChartHub> hubContext)
-        {
-            _hubContext = hubContext;
-                
-        }
-
-        public IActionResult Get()
-        {
-
-            var timerManager = new TimerManager(() =>
-            //send data to all listener clients
-            _hubContext.Clients.All.SendAsync("pustChartData", DataManager.GetChartData())
-            );
-            return Ok(new { Message = "data pust av Chart'n er ferdig" });
-        }
-    }
-}
+﻿using Microsoft.AspNetCore.Http;using Microsoft.AspNetCore.Mvc;using Microsoft.AspNetCore.SignalR;using SignalR.Server.HubConfig;using SignalR.Server.TimerFeatures;using SignalR.Server.DataStorage;namespace SignalR.Server.Kontroller{    [Route("api/[controller]")]    [ApiController]    public class ChartController : ControllerBase    {        private readonly IHubContext<ChartHub> _hubContext;        public ChartController(IHubContext<ChartHub> hubContext)        {            _hubContext = hubContext;                        }        public IActionResult Get()        {            var timerManager = new TimerManager(() =>            //send data to all listener clients            _hubContext.Clients.All.SendAsync("pustChartData", DataManager.GetChartData())            );            return Ok(new { Message = "data pust av Chart'n er ferdig" });        }    }}
--- SignalR.Server/Program.cs
+++ SignalR.Server/Program.cs
@@ -1,4 +1,5 @@
 using SignalR.Server.HubConfig;
+using SignalR.Server.TimerFeatures;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,39 +15,15 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddSignalR();
+builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
-
 app.UseCors("CorsPolicy");
-app.UseEndpoints(endpoints => {
-    endpoints.MapControllers();
-    endpoints.MapHub<ChartHub>("/chart");
-}) ;
+app.MapControllers();
+app.MapHub<ChartHub>("/chart");
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
