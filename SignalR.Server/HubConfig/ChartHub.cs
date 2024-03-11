11a8c737deb842cc86e56f989b275668c1c984f7
sagadavid <epostsaga@gmail.com>
2024-03-11 11:12:36 +0100

    two way communication enabled

--- SignalR.Server/HubConfig/ChartHub.cs
+++ SignalR.Server/HubConfig/ChartHub.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+namespace SignalR.Server.Models
 
 namespace SignalR.Server.HubConfig
 {
     public class ChartHub : Hub
     {
         //when hubcontext is oneway, but not a two-way trip, then there is no need for any mehtod.
+        //hub, becomes two way communicable.. recieves from client and broadcasts to clients
+        public async Task BroadcastChartData(List<ChartModel>data)=>
+            await Clients.All.SendAsync("pustChartData", data);
        
     }
 }
